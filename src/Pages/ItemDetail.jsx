abd8966fc8f3fb72e6b168bea21d572261e84a69
thebigbillz <adenugbafavour@gmail.com>
2022-01-04 14:10:12 +0100

    fixed-api-call

--- src/Pages/ItemDetail.jsx
+++ src/Pages/ItemDetail.jsx
@@ -1,8 +1,24 @@
+import axios from "axios";
 import React from "react";
 import { useParams } from "react-router-dom";
 
 const ItemDetail = () => {
   const { id } = useParams();
+
+  const [item, setItem] = React.useState({});
+
+  React.useEffect(() => {
+    getItem();
+  }, []);
+
+  const getItem = async () => {
+    const response = await axios.get(
+      `https://fortnite-api.theapinetwork.com/item/get?id=${id}`
+    );
+    const itemData = await response.data;
+    console.log(itemData);
+    setItem(itemData);
+  };
 
   return (
     <div>
